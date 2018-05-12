832b8f3e34fbb859e4f5218a0059b5c3ab8c253e
imnumtwo <39208863+imnumtwo@users.noreply.github.com>
2018-05-12 00:47:18 -0400

    Create calc.php

---
+++ calc.php
@@ -0,0 +1,9 @@
+<?php 
+
+         echo "<script language=\"JavaScript\">\n"; 
+         echo "alert('invalid username/password');\n"; 
+         echo "window.location='index.html'"; 
+         echo "</script>"; 
+
+
+?>
