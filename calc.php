9f37e43c227580373efbea23df72453eb9414b26
imnumtwo <39208863+imnumtwo@users.noreply.github.com>
2018-05-12 02:05:23 -0400

    Delete calc.php

--- calc.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-         echo "<script language=\"JavaScript\">\n"; 
-         echo "alert('invalid username/password');\n"; 
-         echo "window.location='index.html'"; 
-         echo "</script>"; 
-
-
-?>
